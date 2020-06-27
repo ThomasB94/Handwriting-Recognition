e0177734ac4e9b917426bb092899df741431b6d7
Paul Hofman <paulwhofman@hotmail.com>
2020-06-27 22:45:39 +0200

    Added image read stuff

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 # regular imports
 import os
 import cv2
+import sys
 
 # our algorithms
 from line_segmentation.textline import textlines
 
 def main():
-    # os stuff to walk through dir.
-
+    path = str(sys.argv[1])
+    print("Using the images found in the folder with path:", path)
+    # get all the files in the directory, this should be the directory with the test images    
+    files = os.listdir(path)
+    # filter out everything that's not a JPEG image
+    #################################################
+    #TODO: MAKE SURE THIS ACTUALLY TAKES CORRECT EXT.
+    #TODO: ALSO MAKE SURE THERE ARE NO SPACES IN THE PATH OR USE \
+    ##################################################
+    files = [f for f in files if f.lower().endswith('.jpg')]
+    print("Found the following files:", files)
+        
     for file_name in files:
         im = cv2.imread(file_name, cv2.IMREAD_GRAYSCALE)
         # INPUT: cv2 grayscale image
@@ -23,5 +34,8 @@ def main():
             #charList = segmChars(line)
         #class.
         #style
+
+if __name__ == "__main__":
+    main()
     
-main()
+
