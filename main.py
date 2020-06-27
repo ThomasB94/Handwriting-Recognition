bffaf179e87dd1dd1a514f6ea9f2d13021a1d4a3
Paul Hofman <paulwhofman@hotmail.com>
2020-06-27 13:09:44 +0200

    Added main

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    #im = imread(image)
+    #alle ims inlezen
+    #for im in ims:
+        #lines = textlines(im)
+        #char seg
+        #class.
+        #style
+
+
+
+
+
+
+
+
+
+main()
