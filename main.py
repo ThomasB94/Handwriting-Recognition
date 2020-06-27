f33388b20735830c83c153185228fab8f624e234
Paul Hofman <paulwhofman@hotmail.com>
2020-06-27 13:16:04 +0200

    Added comment

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ def main():
     #im = imread(image)
     #alle ims inlezen
     #for im in ims:
+        #textline uses image as input and outputs list of rectangles with lines of text
         #lines = textlines(im)
         #char seg
         #class.
         #style
+    
 
 
 
