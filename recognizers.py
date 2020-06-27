fd8bbcb4f2f9b6a40eb5a0726a3bf277ee09ac2f
Daan <daan.lambert@gmail.com>
2020-06-27 13:53:32 +0200

    naming fixed in recognizer

--- recognizers.py
+++ recognizers.py
@@ -8,16 +8,16 @@ from character_recognition import *
 
 
 
-class recognizer:
+class Recognizer:
 
     def __init__(self):
         with open('character_recognizer.pickle', 'rb') as pfile:
             self.char_model = pickle.load(pfile)
         with open('style_classifier.pickle', 'rb') as pfile:
             self.style_model = pickle.load(pfile)
-        self.styleVect = [0, 0, 0]
+        self.style_vect = [0, 0, 0]
         self.styles = ["Archaic", "Hasmonean", "Herodian"]
-        self.featureList = [
+        self.feature_list = [
                     [feature1],
                     [feature2],
                     [feature3],
@@ -28,41 +28,41 @@ class recognizer:
                     [feature8],
                     ]
 
-    def createFeatureVect(self, image):
+    def _create_feature_vect(self, image):
         #uses function from character_recognition
-        return featureVect(image, self.featureList)
+        return featureVect(image, self.feature_list)
 
-    def predictCharacter(self, image_features_vector):
+    def _predict_character(self, image_features_vector):
         return self.char_model.predict(image_features_vector)
 
-    def probabilityCheck(self, image_features_vector):
+    def _probability_check(self, image_features_vector):
         probs = self.char_model.predict_proba(image_features_vector)
         print(probs)
 
-    def predictStyle(self, image_features_vector):
+    def _predict_style(self, image_features_vector):
         return self.style_model.predict(image_features_vector)
 
-    def recordStyle(self, style):
+    def _record_style(self, style):
         #records the style in the class
-        self.styleVect[self.styles.index(style)] = self.styleVect[self.styles.index(style)] + 1
+        self.style_vect[self.styles.index(style)] = self.style_vect[self.styles.index(style)] + 1
 
-    def getStyle(self):
-        return self.styles[np.argmax(self.styleVect)]
-        self.pageReset()
+    def get_style(self):
+        return self.styles[np.argmax(self.style_vect)]
+        self.page_reset()
 
-    def pageReset(self):
-        self.styleVect = [0, 0, 0]
+    def _page_reset(self):
+        self.style_vect = [0, 0, 0]
 
     def predict(self, image):
-        image_features = self.createFeatureVect(image).reshape(1, -1)
-        char = self.predictCharacter(image_features)[0]
-        style = self.predictStyle(image_features)[0]
-        self.recordStyle(style)
+        image_features = self._create_feature_vect(image).reshape(1, -1)
+        char = self._predict_character(image_features)[0]
+        style = self._predict_style(image_features)[0]
+        self._record_style(style)
         return char
 
-predictor = recognizer()
-image = cv2.imread("./characters_for_style_classification/Archaic/Alef/Alef_00.jpg", cv2.IMREAD_GRAYSCALE)
+predictor = Recognizer()
+image = cv2.imread("./characters_for_style_classification/Hasmonean/Bet/Bet_00.jpg", cv2.IMREAD_GRAYSCALE)
 _, image = cv2.threshold(image,127,255,cv2.THRESH_BINARY)
 print(image.shape)
-predictor.predict(image)
-print(predictor.getStyle())
+print(predictor.predict(image))
+print(predictor.get_style())
